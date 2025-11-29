eb71ec41b6c3a9fcd06b5a5ce11a5dd8ba40c2dc
Ramil <zarram89@gmail.com>
2025-11-29 08:43:17 +0500

    feat: adds react router

---
+++ project/src/components/private-route/PrivateRoute.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Navigate } from 'react-router-dom';
+import { AppRoute, AuthorizationStatus } from '../../const';
+
+type PrivateRouteProps = {
+  authorizationStatus: AuthorizationStatus;
+  children: JSX.Element;
+};
+
+function PrivateRoute({ authorizationStatus, children }: PrivateRouteProps): JSX.Element {
+  return (
+    authorizationStatus === AuthorizationStatus.Auth
+      ? children
+      : <Navigate to={AppRoute.SignIn} />
+  );
+}
+
+export default PrivateRoute;
--- project/src/pages/MyListScreen/MyListScreen.tsx
+++ project/src/pages/MyListScreen/MyListScreen.tsx
@@ -21,7 +21,7 @@ function MyListScreen(): JSX.Element {
             </div>
           </li>
           <li className="user-block__item">
-            <a className="user-block__link">Sign out</a>
+            <a className="user-block__link" href="index.html">Sign out</a>
           </li>
         </ul>
       </header>
